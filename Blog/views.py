78f8aa7ffcd23325a996a06562ef79e9d9b6c318
fatmajanna <fatma.janna.utp@gmail.com>
2019-02-13 20:04:34 +0700

    Substitute project_views with the project_advance_views

--- Blog/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
-def display_blog(request):
-    return render(request, 'Blog/blog.html', {})
--- Halaman_Author/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
-def display_author(request):
-    return render(request, 'Halaman_Author/author.html', {})
